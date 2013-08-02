75780902df341d2deb9b85dd91bd2cab383a85a0
The Law <email@example.fake>
2013-08-01 18:35:09 -0700

    Finished user signup. Lesson 8, conclusion

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -4,7 +4,6 @@ describe UsersController do
   render_views
 
   describe "GET 'show'" do
-
     before(:each) do 
       @user = Factory(:user)
     end
@@ -52,4 +51,60 @@ describe UsersController do
       response.should have_selector('title', :content => "Sign up")
     end
   end
-end
+
+  describe "POST 'create'" do
+    describe "failure" do
+      before(:each) do
+        @attr = {
+          :name => "",
+          :email => "", 
+          :password => "", 
+          :password_confirmation => ""
+        }
+      end
+      it "should have the right title" do
+        post :create, :user => @attr
+        response.should have_selector('title', :content => "Sign up")
+      end
+      it "should render the 'new' page" do
+        post :create, :user => @attr
+        response.should render_template('new')
+      end
+      #if you post to the signup with invalid attributes, it should not create a user
+      it "should not create a user" do
+        lambda do
+          post :create, :user => @attr
+        end.should_not change(User, :count)
+      end
+    end #end create failure
+
+    describe "success" do
+      before(:each) do
+        @attr = {
+          :name => "New User",
+          :email => "user@example.com", 
+          :password => "foobar", 
+          :password_confirmation => "foobar"
+        }
+      end
+
+      it "should create a user" do
+        lambda do
+          post :create, :user => @attr
+        end.should change(User, :count).by(1)
+      end
+
+      it "should redirect to the user's show page" do
+        post :create, :user => @attr
+        response.should redirect_to(user_path(assigns(:user)))
+      end
+
+      it "should have a welcome message" do
+        post :create, :user => @attr
+        flash[:success].should =~ /welcome to the sample app/i
+      end
+
+    end #end describe success
+  end #end POST 'create'
+end #end describe UserController
+
