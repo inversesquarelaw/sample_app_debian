e93e0e270ddcdd05ddc5655080192a6e4e635bc0
The Law <email@example.fake>
2013-08-09 18:25:32 -0700

    Microposts interface. Lesson 11, Manipulating Microposts, 32:00.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -51,11 +51,6 @@ class UsersController < ApplicationController
 
   private
 
-    def authenticate
-      deny_access unless signed_in?
-      #deny_access is located in sessions_helper file
-    end
-
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_path) unless current_user?(@user)
